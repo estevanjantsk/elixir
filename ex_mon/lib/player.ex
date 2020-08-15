3b48db00bdb65e0fcddb4ccc4fcf9825a7a4610e
Estevan Jantsk <estevan.jantsk@gmail.com>
2020-08-15 19:58:49 -0300

    verify player move

--- ex_mon/lib/player.ex
+++ ex_mon/lib/player.ex
@@ -1,5 +1,5 @@
 defmodule ExMon.Player do
-  @required_keys [:life, :move_avg, :move_heal, :move_rnd, :name]
+  @required_keys [:life, :moves, :name]
   @max_file 100
 
   @enforce_keys @required_keys
@@ -8,9 +8,11 @@ defmodule ExMon.Player do
   def build(name, move_avg, move_rnd, move_heal) do
     %ExMon.Player{
       life: @max_file,
-      move_avg: move_avg,
-      move_heal: move_heal,
-      move_rnd: move_rnd,
+      moves: %{
+        move_avg: move_avg,
+        move_heal: move_heal,
+        move_rnd: move_rnd
+      },
       name: name
     }
   end
