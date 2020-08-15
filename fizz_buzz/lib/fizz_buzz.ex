b86104ad7bb5c3b73d6a098d6c1d4bfeed9bad7e
Estevan Jantsk <estevan.jantsk@gmail.com>
2020-08-15 09:16:45 -0300

    substituindo os elementos da lista

--- fizz_buzz/lib/fizz_buzz.ex
+++ fizz_buzz/lib/fizz_buzz.ex
@@ -8,7 +8,12 @@ defmodule FizzBuzz do
   def handle_file_read({:ok, result}) do
     result
     |> String.split(",")
-    |> Enum.map(&String.to_integer/1)
+    |> Enum.map(&convert_and_evaluate_numbers/1)
   end
   def handle_file_read({:error, reason}), do: "Error reading the file due to: #{reason}"
+
+  def convert_and_evaluate_numbers(elem) do
+    elem
+    |> String.to_integer()
+  end
 end
