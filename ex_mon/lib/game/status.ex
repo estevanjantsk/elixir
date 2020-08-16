d1f1b3a54a23b7f840608f79546936a07e258755
Estevan Jantsk <estevan.jantsk@gmail.com>
2020-08-16 17:06:10 -0300

    add print_move_message

--- ex_mon/lib/game/status.ex
+++ ex_mon/lib/game/status.ex
@@ -9,4 +9,11 @@ defmodule ExMon.Game.Status do
   def print_wrong_move_message(move) do
     IO.puts("\n ===== Invalid move: #{move} =====\n")
   end
+
+  def print_move_message(:computer, :attack, damage) do
+    IO.puts("\n ===== The player attacked the computer dealing #{damage} damage! =====\n")
+  end
+  def print_move_message(:player, :attack, damage) do
+    IO.puts("\n ===== The computer attacked the player dealing #{damage} damage! =====\n")
+  end
 end
