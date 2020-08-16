3d83de7e97e6ec46b3c758516ea6faba3a10298e
Estevan Jantsk <estevan.jantsk@gmail.com>
2020-08-15 21:15:21 -0300

    selecionando mov de cura e ataque

--- ex_mon/lib/ex_mon.ex
+++ ex_mon/lib/ex_mon.ex
@@ -1,6 +1,6 @@
 defmodule ExMon do
   alias ExMon.{Game, Player}
-  alias ExMon.Game.Status
+  alias ExMon.Game.{Status, Actions}
 
   @computer_name "Robotinik"
 
@@ -14,5 +14,19 @@ defmodule ExMon do
     |> Game.start(player)
 
     Status.print_round_message()
+  end
+
+  def make_move(move) do
+    move
+    |> Actions.fetch_move()
+    |> do_move()
+  end
+
+  defp do_move({:error, move}), do: Status.print_wrong_move_message(move)
+  defp do_move({:ok, move}) do
+    case move do
+      :move_heal -> "realiza_cura"
+      move -> "ataca"
+    end
   end
 end
