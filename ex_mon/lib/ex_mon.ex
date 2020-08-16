491d4c9fb3a35d34a92e1ceca1858f5aa07fa63f
Estevan Jantsk <estevan.jantsk@gmail.com>
2020-08-16 10:57:06 -0300

    calculate_total_life that opponent should receive

--- ex_mon/lib/ex_mon.ex
+++ ex_mon/lib/ex_mon.ex
@@ -26,7 +26,7 @@ defmodule ExMon do
   defp do_move({:ok, move}) do
     case move do
       :move_heal -> "realiza_cura"
-      move -> "ataca"
+      move -> Actions.attack(move)
     end
   end
 end
