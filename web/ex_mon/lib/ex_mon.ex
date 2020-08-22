a3255d34f8e23aa5d45484da5d4005dc8554259f
Estevan Jantsk <estevan.jantsk@gmail.com>
2020-08-22 11:46:46 -0300

    Update trainer logic

--- web/ex_mon/lib/ex_mon.ex
+++ web/ex_mon/lib/ex_mon.ex
@@ -2,6 +2,7 @@ defmodule ExMon do
   alias ExMon.Trainer
 
   defdelegate create_trainer(params), to: Trainer.Create, as: :call
+  defdelegate update_trainer(params), to: Trainer.Update, as: :call
   defdelegate delete_trainer(id), to: Trainer.Delete, as: :call
   defdelegate fetch_trainer(id), to: Trainer.Get, as: :call
 end
