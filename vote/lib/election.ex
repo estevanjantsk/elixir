6166a05645701caf5c44e1d70d98267769bfa01d
Estevan Jantsk <estevan.jantsk@gmail.com>
2020-09-06 14:58:12 +0300

    First App View

--- vote/lib/election.ex
+++ vote/lib/election.ex
@@ -5,4 +5,10 @@ defmodule Election do
               Candidate.new(2, "Candidato 2")
             ],
             next_id: 3
+
+  def view_header(election) do
+    [
+      "Election for: #{election.name}\n"
+    ]
+  end
 end
