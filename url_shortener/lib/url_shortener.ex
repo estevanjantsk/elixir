464a915ceb1245622cb0c33adba22982daab8722
Estevan Jantsk <estevan.jantsk@gmail.com>
2020-09-12 21:29:49 +0300

    add flush and count

--- url_shortener/lib/url_shortener.ex
+++ url_shortener/lib/url_shortener.ex
@@ -3,15 +3,23 @@ defmodule URLShortener do
 
   # Client API
   def start_link(opts \\ []), do: GenServer.start(__MODULE__, :ok, opts)
+
   def stop(pid), do: GenServer.cast(pid, :stop)
-  def shorten(pid, url), do: GenServer.cast(pid, {:shorten, url})
+  def flush(pid), do: GenServer.cast(pid, :flush)
+
+  def shorten(pid, url), do: GenServer.call(pid, {:shorten, url})
   def get(pid, short_link), do: GenServer.call(pid, {:get, short_link})
+  def count(pid), do: GenServer.call(pid, {:count})
 
   # GenServer callbacks
   def init(:ok), do: {:ok, %{}}
 
   def handle_cast(:stop, state) do
     {:stop, :normal, state}
+  end
+
+  def handle_cast(:flush, _state) do
+    {:noreply, %{}}
   end
 
   def handle_call({:shorten, url}, _from, state) do
@@ -21,6 +29,10 @@ defmodule URLShortener do
 
   def handle_call({:get, short_link}, _from, state) do
     {:reply, Map.get(state, short_link), state}
+  end
+
+  def handle_call({:count}, _from, state) do
+    {:reply, Enum.count(state), state}
   end
 
   defp md5(url) do
