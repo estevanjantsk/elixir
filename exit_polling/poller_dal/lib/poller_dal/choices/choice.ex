c611fd4fc8a098c8b9fed84dda0c32b03013ebaf
Estevan Jantsk <estevan.jantsk@gmail.com>
2020-10-03 22:11:38 +0300

    add parties and party_ids

--- exit_polling/poller_dal/lib/poller_dal/choices/choice.ex
+++ exit_polling/poller_dal/lib/poller_dal/choices/choice.ex
@@ -2,6 +2,12 @@ defmodule PollerDal.Choices.Choice do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @parties [
+    {"Democrat", 1},
+    {"Republican", 2}
+  ]
+  @parties_id Enum.map(@parties, fn {_, id} -> id end)
+
   schema "choices" do
     field(:description, :string)
     field(:votes, :integer, default: 0)
@@ -15,6 +21,17 @@ defmodule PollerDal.Choices.Choice do
     choice
     |> cast(attrs, [:description, :question_id, :party, :votes])
     |> validate_required([:description, :question_id, :party])
+    |> validate_inclusion(:party, @parties_id)
     |> assoc_constraint(:question)
+  end
+
+  def parties(), do: @parties
+  def party_ids(), do: @parties_id
+
+  def party_description(id) do
+    case Enum.find(@parties, fn {_, party_id} -> party_id == id end) do
+      nil -> ""
+      {description, _} -> description
+    end
   end
 end
