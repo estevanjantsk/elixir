bebd006cd704079a270534ded6602b461a735b77
Estevan Jantsk <estevan.jantsk@gmail.com>
2020-10-04 22:42:17 +0300

    module attributes

--- servy/lib/servy/handler.ex
+++ servy/lib/servy/handler.ex
@@ -1,4 +1,9 @@
 defmodule Servy.Handler do
+  @moduledoc "Handles HTTP requests."
+
+  @pages_path Path.expand("../../pages", __DIR__)
+
+  @doc "Transforms the request into a response."
   def handle(request) do
     request
     |> parse
@@ -9,6 +14,7 @@ defmodule Servy.Handler do
     |> format_response
   end
 
+  @doc "Logs 404 requests"
   def track(%{status: 404, path: path} = conv) do
     IO.puts("Warning: #{path} is on the loose!")
     conv
@@ -38,30 +44,19 @@ defmodule Servy.Handler do
     %{conv | status: 200, resp_body: "Bears, Lions, Tigers"}
   end
 
-  def route(%{method: "GET", path: "/about"} = conv) do
-    Path.expand("../../pages", __DIR__)
-    |> Path.join("about.html")
-    |> File.read()
-    |> handle_file(conv)
-
-    #   case File.read(file) do
-    #     {:ok, content} ->
-    #       %{conv | status: 200, resp_body: content}
-
-    #     {:error, :enoent} ->
-    #       %{conv | status: 404, resp_body: "File not found!"}
-
-    #     {:error, reason} ->
-    #       %{conv | status: 500, resp_body: "File error: #{reason}"}
-    #   end
-  end
-
   def route(%{method: "GET", path: "/bears"} = conv) do
     %{conv | status: 200, resp_body: "Teddy, Smokey, Paddington"}
   end
 
   def route(%{method: "GET", path: "/bears/" <> id} = conv) do
     %{conv | status: 200, resp_body: "Bear #{id}"}
+  end
+
+  def route(%{method: "GET", path: "/about"} = conv) do
+    @pages_path
+    |> Path.join("about.html")
+    |> File.read()
+    |> handle_file(conv)
   end
 
   def route(%{path: path} = conv) do
