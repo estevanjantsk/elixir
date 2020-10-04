f5236ed35d88bbdcbe870af97eaef3b141fa3ec2
Estevan Jantsk <estevan.jantsk@gmail.com>
2020-10-04 20:49:00 +0300

    route and response

--- servy/lib/servy/handler.ex
+++ servy/lib/servy/handler.ex
@@ -17,18 +17,16 @@ defmodule Servy.Handler do
   end
 
   def route(conv) do
-    # TODO: Create a new map that also has the response body:
-    conv = %{method: "GET", path: "/wildthings", resp_body: "Bears, Lions, Tigers"}
+    %{conv | resp_body: "Bears, Lions, Tigers"}
   end
 
   def format_response(conv) do
-    # TODO: Use values in the map to create an HTTP response string:
     """
     HTTP/1.1 200 OK
     Content-Type: text/html
-    Content-Length: 20
+    Content-Length: #{String.length(conv.resp_body)}
 
-    Bears, Lions, Tigers
+    #{conv.resp_body}
     """
   end
 end
