b494ea0d7c4ba88504f060648bc78683843cd82a
Estevan Jantsk <estevan.jantsk@gmail.com>
2020-10-04 19:20:57 +0300

    parse request line

--- servy/lib/servy/handler.ex
+++ servy/lib/servy/handler.ex
@@ -7,8 +7,13 @@ defmodule Servy.Handler do
   end
 
   def parse(request) do
-    # TODO: Parse the request string into a map:
-    conv = %{method: "GET", path: "/wildthings", resp_body: ""}
+    [method, path, _] =
+      request
+      |> String.split("\n")
+      |> List.first()
+      |> String.split(" ")
+
+    %{method: method, path: path, resp_body: ""}
   end
 
   def route(conv) do
