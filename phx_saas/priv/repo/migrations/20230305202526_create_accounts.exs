4c6d4b99d0ea7f0e8e45f2c2e3e1222cd00f8026
Estevan Jantsk <estevan.jantsk@gmail.com>
2023-03-05 22:32:33 +0200

    Create Accounts table and logic

--- phx_saas/priv/repo/migrations/20230305202526_create_accounts.exs
+++ phx_saas/priv/repo/migrations/20230305202526_create_accounts.exs
@@ -5,11 +5,11 @@ defmodule PhxSaas.Repo.Migrations.CreateAccounts do
     create table(:accounts) do
       add :name, :string
       add :personal, :boolean, default: false, null: false
-      add :created_by_user_id, references(:users, on_delete: :nothing)
+      add :created_by_user_id, references(:users, on_delete: :nilify_all)
 
       timestamps()
     end
 
-    create index(:accounts, [:created_by_user_id])
+    create unique_index(:accounts, [:created_by_user_id], name: "accounts_limit_personal_index", where: "personal = true")
   end
 end
